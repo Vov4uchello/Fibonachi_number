6205dfd0a6dbfa4017c9b9d0c7aabef7754d3a7a
Vov4uchello <romavovchuk99@gmail.com>
2019-03-01 13:22:21 +0200

    first commit

---
+++ Fibonachi_Number/Fibonachi_Number.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int b = 1;
+
+int fib(int n, int b) {
+
+	if (n > 2) {
+		b++;		
+		b = fib(n - 1, b) + fib(n - 2, b);
+	}
+
+	if (n == 2 || n == 1) {
+		return 1;
+	}
+	
+	if (n <= 0 ) {
+		cout << "n <= ";
+		return 0;
+	}
+
+	else return b;
+}
+
+int main()
+{
+	int a;
+	cin >> a;
+	int num;
+	num = fib(a, b);
+    cout << num << endl; 
+	system("pause");
+	return 0;
+}
